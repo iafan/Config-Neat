b206234f3a895b23a746d06b67f7ce692f66bd6e
afan <afan@evernote.com>
2014-04-11 19:06:12 +0400

    Bump version to 0.7

--- perl/lib/Config/Neat.pm
+++ perl/lib/Config/Neat.pm
@@ -71,7 +71,7 @@ L<https://github.com/iafan/Config-Neat>
 
 package Config::Neat;
 
-our $VERSION = '0.6';
+our $VERSION = '0.7';
 
 use strict;
 
--- perl/lib/Config/Neat/Schema.pm
+++ perl/lib/Config/Neat/Schema.pm
@@ -46,7 +46,7 @@ any arbitrary data structure and are not validated.
 
 package Config::Neat::Schema;
 
-our $VERSION = '0.6';
+our $VERSION = '0.7';
 
 use strict;
 
